46d113882f859526dcc62197a6311d66faca0ade
Ally Netzel <netzelally@gmail.com>
2018-05-25 22:54:44 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,12 +1,13 @@
 class Application
 
+@@items = [Item.new("Figs",3.42), Item.new("Pears",0.99)]
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
-      if item = @@items.find{|i| i.name == item.name}
+      if item = @@items.find{|i| i.name == item_name}
         resp.write "#{item.price}"
       else
         resp.write "Item not found"
